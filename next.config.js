9a9d078b614df5ba12b3233c59fe2ea24b22f1a8
Mircea Petre <petermir44@gmail.com>
2023-01-18 21:29:42 +0200

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   images: { domains: ['res.cloudinary.com'] },
-  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
